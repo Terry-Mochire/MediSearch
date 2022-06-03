001103eea93210015c54f4c463fa1be4d80f4a74
Terry <terrybmochire@gmail.com>
2022-06-03 19:15:35 +0300

    Add toasts.

--- app/src/main/java/com/example/medi_search/OnboardingActivity.java
+++ app/src/main/java/com/example/medi_search/OnboardingActivity.java
@@ -4,14 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class OnboardingActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class OnboardingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.signUp) Button mSignUp;
+    @BindView(R.id.userName) EditText mUserName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
-
+        ButterKnife.bind(this);
         Intent intent = getIntent();
+        mSignUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String userName = mUserName.getText().toString();
+        Toast.makeText(OnboardingActivity.this, "Welcome to Medi-Search " + userName, Toast.LENGTH_LONG).show();
     }
 }
