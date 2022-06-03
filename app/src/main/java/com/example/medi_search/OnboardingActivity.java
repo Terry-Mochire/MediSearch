cceae587192c89f3f5d93b5083784d25d6d1d0e2
Terry <terrybmochire@gmail.com>
2022-06-03 18:31:26 +0300

    Change styling onboarding activity and navigate to it from main activity.

--- app/src/main/java/com/example/medi_search/OnboardingActivity.java
+++ app/src/main/java/com/example/medi_search/OnboardingActivity.java
@@ -2,6 +2,7 @@ package com.example.medi_search;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class OnboardingActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        Intent intent = getIntent();
     }
 }
