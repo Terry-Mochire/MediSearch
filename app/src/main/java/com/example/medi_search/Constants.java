4d204c417025e36a1a3a30b28fdef6d6f36f9567
Terry <terrybmochire@gmail.com>
2022-06-06 17:43:06 +0300

    Update gitignore and add constants class.

---
+++ app/src/main/java/com/example/medi_search/Constants.java
@@ -0,0 +1,7 @@
+package com.example.medi_search;
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_ID = BuildConfig.API_ID;
+    public static final String API_BASE_URL = "https://api.infermedica.com/v3";
+}
