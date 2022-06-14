e524018bb3bac8b377a6f3ad7bd7a5129af955df
Terry <terrybmochire@gmail.com>
2022-06-15 01:44:35 +0300

    Write function to register account to firebase.

--- app/src/main/java/com/example/medi_search/ui/OnboardingActivity.java
+++ app/src/main/java/com/example/medi_search/ui/OnboardingActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.medi_search.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,15 +20,22 @@ public class OnboardingActivity extends AppCompatActivity implements View.OnClic
 
     @BindView(R.id.signUp) Button mSignUp;
     @BindView(R.id.userName) EditText mUserName;
+    @BindView(R.id.userEmail) EditText mUserEmail;
+    @BindView(R.id.userPassword) EditText mUserPassword;
+    @BindView(R.id.userConfirmPassword) EditText mUserConfirmPassword;
     @BindView(R.id.directToSignIn) Button mdirectToSignIn;
 
-
+    private FirebaseAuth mAuth;
+    public static final String TAG = OnboardingActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
         ButterKnife.bind(this);
         Intent intent = getIntent();
+
+        mAuth = FirebaseAuth.getInstance();
+
         mSignUp.setOnClickListener(this);
         mdirectToSignIn.setOnClickListener(this);
 
@@ -35,6 +44,7 @@ public class OnboardingActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View v) {
        if (v == mSignUp ) {
+           createNewUser();
            String userName = mUserName.getText().toString();
            Toast.makeText(OnboardingActivity.this, "Welcome to Medi-Search " + userName, Toast.LENGTH_LONG).show();
            Intent intent = new Intent(OnboardingActivity.this, StartAssessmentActivity.class);
@@ -49,5 +59,21 @@ public class OnboardingActivity extends AppCompatActivity implements View.OnClic
            startActivity(intent);
            finish();
        }
+    }
+
+    private void createNewUser() {
+        final String name = mUserName.getText().toString().trim();
+        final String email = mUserEmail.getText().toString().trim();
+        String password = mUserPassword.getText().toString().trim();
+        String confirmPassword = mUserConfirmPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Authentication is successful!");
+                    } else {
+                        Toast.makeText(OnboardingActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
