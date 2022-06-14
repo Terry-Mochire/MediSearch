78c20057fee34d4ccb4f30356ed36c749f9976e1
Terry <terrybmochire@gmail.com>
2022-06-15 01:23:54 +0300

    Navigate to onboarding activity from sign in activity.

--- app/src/main/java/com/example/medi_search/ui/SignInActivity.java
+++ app/src/main/java/com/example/medi_search/ui/SignInActivity.java
@@ -4,15 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.medi_search.R;
 
-public class SignInActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.askToRegister)
+    TextView mAskToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         Intent intent = getIntent();
+        ButterKnife.bind(this);
+        mAskToRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAskToRegister) {
+            Intent intent = new Intent(SignInActivity.this, OnboardingActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
