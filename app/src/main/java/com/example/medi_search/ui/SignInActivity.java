4ff790d43f13b3683b7c01d17f6be9b71fed4314
Terry <terrybmochire@gmail.com>
2022-06-15 04:10:14 +0300

    Add sign in functionality.

--- app/src/main/java/com/example/medi_search/ui/SignInActivity.java
+++ app/src/main/java/com/example/medi_search/ui/SignInActivity.java
@@ -1,21 +1,34 @@
 package com.example.medi_search.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.medi_search.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.askToRegister)
-    TextView mAskToRegister;
+    @BindView(R.id.askToRegister) TextView mAskToRegister;
+    @BindView(R.id.signInEmail) TextView mSignInEmail;
+    @BindView(R.id.signInPassword) TextView mSignInPassword;
+    @BindView(R.id.SignIn) Button mSignInButton;
+
+    public static final String TAG = SignInActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,10 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_in);
         Intent intent = getIntent();
         ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
         mAskToRegister.setOnClickListener(this);
+        mSignInButton.setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +49,37 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             startActivity(intent);
             finish();
         }
+        if(v == mSignInButton) {
+            login();
+        }
+    }
+
+    private void login() {
+        String email = mSignInEmail.getText().toString().trim();
+        String password = mSignInPassword.getText().toString().trim();
+
+        if (email.equals("")){
+            mSignInEmail.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mSignInPassword.setError("Password cannot be blank");
+            return;
+        }
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        });
     }
 }
