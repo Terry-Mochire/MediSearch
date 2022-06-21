fa3110aace83967486250016a38345fa6d11bb57
Terry <terrybmochire@gmail.com>
2022-06-21 19:15:41 +0300

    Style profile page.

---
+++ app/src/main/java/com/example/medi_search/ui/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.medi_search.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medi_search.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
